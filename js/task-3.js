bd7809dcab6cf30cd0f65f3c66efc6c4fd740ce4
Bohdan <toyvo7@gmail.com>
2025-01-20 20:43:27 +0100

    modify task3

--- js/task-3.js
+++ js/task-3.js
@@ -4,7 +4,7 @@
 
 function filterArray(numbers, value) {
   const newArr = [];
-  for (let i = 0; i <= numbers.length; i++) {
+  for (let i = 0; i < numbers.length; i++) {
     if (numbers[i] > value) {
       newArr.push(numbers[i]);
     }
